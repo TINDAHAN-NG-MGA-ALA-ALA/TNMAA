a147db514db7bc7b357fa662e79d45e4449e8ac3
hulyusynthesis <fabiosa.juliusjanven07@gmail.com>
2026-05-13 14:03:41 +0800

    Initial skeleton

---
+++ app.py
@@ -0,0 +1,7 @@
+﻿from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Hello from Tindahan Team – under construction"
+if __name__ == '__main__':
+    app.run(debug=True)
